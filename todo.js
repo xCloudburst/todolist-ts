d987e56480f8f3cddaca1f46db9842127739bcac
xCloudburst <jvip7337@gmail.com>
2021-08-24 17:20:21 +0200

    that's me for today

--- todo.js
+++ todo.js
@@ -1,29 +1,34 @@
-let button = document.querySelector('.button'),
-    list = "",
-    newDiv = "",
-    newContent = "",
-    newButton = "",
-    counter = 0,
-    i = -1;
+let button = document.querySelector(".button"),
+  list = "",
+  newDiv = "",
+  newContent = "",
+  newButton = "",
+  counter = 0,
+  i = -1;
 
-todoText.addEventListener("keyup", function(textBox) {
-    textBox.preventDefault();
-        if (textBox.keyCode === 13) {
-            createLineItem()
-        }
+todoText.addEventListener("keyup", function (textBox) {
+  textBox.preventDefault();
+  if (textBox.keyCode === 13) {
+    createLineItem();
+  }
 });
 
-function createLineItem(){
+function createLineItem() {
+  if (document.getElementById("todoText").value === "") {
+  } else {
     newDiv = document.createElement("div");
     newButton = document.createElement("button");
-    newContent = document.createTextNode(document.getElementById("todoText").value);
+    newContent = document.createTextNode(
+      document.getElementById("todoText").value
+    );
     newDiv.appendChild(newContent);
     list = document.getElementById(item);
     document.body.insertBefore(newDiv, list);
     document.body.insertBefore(newButton, list);
-    document.getElementById('todoText').value = "";
-};
+    document.getElementById("todoText").value = "";
+  }
+}
 
-function bgChange(color){
-    document.body.style.background = color;
-};
+function bgChange(color) {
+  document.body.style.background = color;
+}
